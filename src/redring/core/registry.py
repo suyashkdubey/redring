9feeed003861c8a464c62a610bbfe418a7427e56
suyash <suyashxdev@gmail.com>
2026-07-29 19:26:21 +0530

    feat(core): Add registry with register, get and list classmethods

---
+++ src/redring/core/registry.py
@@ -0,0 +1,16 @@
+from .scanner import BaseScanner
+
+class ScannerRegistry():
+    _registry: dict[str, type[BaseScanner]] = {}
+
+    @classmethod
+    def register(cls, scanner: type[BaseScanner]):
+        cls._registry[scanner.capability()] = scanner
+
+    @classmethod
+    def get(cls, capability: str) -> type[BaseScanner] | None:
+        return cls._registry.get(capability)
+
+    @classmethod
+    def list(cls):
+        return list(cls._registry.keys())
