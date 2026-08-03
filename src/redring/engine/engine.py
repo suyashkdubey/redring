1bb8139a92a75372c553273b92c18252ba59c6c3
suyash <suyashxdev@gmail.com>
2026-08-03 11:52:49 +0530

    chore: change file permissions

---
+++ src/redring/engine/engine.py
@@ -0,0 +1,11 @@
+from redring.core.registry import ScannerRegistry
+from redring.core.models.result import ScanResult
+
+class Engine:
+    def run(self, stack:str) -> list[ScanResult]:
+        scanners = ScannerRegistry.get_by_prefix(stack)
+        results = []
+        for scanner in scanners:
+            scanner_instance = scanner()
+            results.append(scanner_instance.scan())
+        return results
